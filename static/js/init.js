6286e85ea3930bd89a47e75f8d8a2bd29dd445eb
popoffww <popoffww@yandex.ru>
2024-03-07 20:24:22 +0600

    Blogengine-2 with Materialize

--- static/js/init.js
+++ static/js/init.js
@@ -1,4 +1,20 @@
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
      let elems = document.querySelectorAll('.sidenav')
      M.AutoInit();
  })
+
+//document.addEventListener('DOMContentLoaded', function() {
+//     let elems = document.querySelectorAll('.dropdown-trigger')
+//     M.AutoInit();
+// })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+        coverTrigger: false,
+        closeOnClick: true,
+        hover: false,
+        inDuration: 200,
+        outDuration: 1000,
+    });
+  });
